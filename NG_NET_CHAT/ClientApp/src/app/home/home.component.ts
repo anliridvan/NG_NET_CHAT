ad741ade6187ffefac0a35396ab0c2401345e092
anliridvan <anliridvan@hotmail.com>
2022-10-26 15:02:27 +0200

    Client Init A bit

--- NG_NET_CHAT/ClientApp/src/app/home/home.component.ts
+++ NG_NET_CHAT/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  //private connection: signalR.HubConnection;
+  messages: any;
+  connection: any;
+  hideJoin: boolean | undefined;
+  userName: string | undefined;
+  ngOnInit(): void {
+    this.initWebSocket();
+  }
+
+  initWebSocket() {
+    this.connection = new HubConnectionBuilder()
+      .withUrl('/hub/chat')
+      .build();
+
+    this.connection.on('messageReceived', (from: string, body: string) => {
+      this.messages.push({ from, body });
+    });
+
+    this.connection.on('userJoined', (user: string) => {
+      if (user === this.userName) {
+        this.hideJoin = true;
+      }
+      this.messages.push({ from: '> ', body: user + ' joined' });
+    });
+
+    this.connection.on('userLeft', (user: string) => {
+      this.messages.push({ from: '! ', body: user + ' has left!' });
+    });
+  }
 }

818d8b1e66eb173d9d3846bd74d7056f84309c17
anliridvan <anliridvan@hotmail.com>
2022-10-26 11:59:24 +0200

    Angular 14.0.3 - .Net 6 - VS INIT

---
+++ NG_NET_CHAT/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NG_NET_CHAT.Models;
+
+namespace NG_NET_CHAT.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}

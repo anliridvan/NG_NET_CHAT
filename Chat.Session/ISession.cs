39b23afd83eaa6110f2020cfbccd17b34a6616a1
anliridvan <anliridvan@hotmail.com>
2022-11-19 20:32:14 +0100

    Sessin Layer Basic Inits

---
+++ Chat.Session/ISession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Session
+{
+    public interface ISession
+    {
+        Guid UserId { get; set; }
+        Guid RoleId { get; set; }
+        string UserName { get; set; }
+        string RoleName { get; set; }
+    }
+}
---
+++ Chat.Session/Session.cs
@@ -0,0 +1,10 @@
+﻿namespace Chat.Session
+{
+    public class Session : ISession
+    {
+        public Guid UserId { get; set; }
+        public Guid RoleId { get; set; }
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
